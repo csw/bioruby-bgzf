856849eb08ce075b8751f684dfd7127c07222334
Clayton Wheeler <cswh@umich.edu>
2012-07-30 16:47:05 -0400

    Added YARD docs.
    [ci skip]

--- lib/bio-bgzf/reader.rb
+++ lib/bio-bgzf/reader.rb
@@ -10,14 +10,32 @@ module Bio::BGZF
       @cur_block = nil
     end
 
+    # Returns the reader's current virtual offset. Between
+    # {#read_block} calls, the file position will always be at the
+    # start of a block or at EOF, so the low 16 bits of the virtual
+    # offset will always be zero.
+    #
+    # @return [Integer] virtual offset for current position
     def tell
       f.tell << 16
     end
 
+    # Reads the BGZF block at the current position. Returns its
+    # decompressed data.
+    #
+    # @return [String] decompressed block data
     def read_block
       decompress_block(f)
     end
 
+    # Reads a portion of a BGZF block, starting from the given virtual
+    # offset. If the offset is the start of a block (low 16 bits are
+    # zero) the entire block's data will be returned. Otherwise, the
+    # subset of the data starting at the given offset will be
+    # returned.
+    #
+    # @param [Integer] vo virtual offset to start from
+    # @return [String] decompressed block data
     def read_block_at(vo)
       block_offset = vo_block_offset(vo)
       data_offset = vo_data_offset(vo)
