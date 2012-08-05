7cffe7ffe4deb295c17877ab1c40645568f0b9e8
Clayton Wheeler <cswh@umich.edu>
2012-08-05 18:38:14 -0400

    Allow caller to set compression level and get last write offset.
    This last is useful for building an index on the starting positions of
    records as they are written. Due to buffering and blocking behavior
    these are not necessarily the same as the positions reported by
    calling #tell before each #write call.

--- lib/bio-bgzf/writer.rb
+++ lib/bio-bgzf/writer.rb
@@ -1,13 +1,21 @@
+require 'zlib'
+
 module Bio::BGZF
   
   class Writer
     include Bio::BGZF
 
-    attr_reader :f, :buf
+    attr_reader :f, :buf, :level
 
-    def initialize(f)
+    # Return the virtual offset of the last {#write} call. This is a
+    # hook for e.g. building an index on the fly.
+    attr_reader :last_write_pos
+
+    def initialize(f, level=2)
       @f = f
+      @level = level
       @buf = ''
+      @buf_write_pos = 0
       if block_given?
         begin
           yield self
@@ -24,15 +32,22 @@ module Bio::BGZF
     def write_buf
       if buf.size > 0
         raise "Buffer too large: #{buf.bytesize}" if buf.bytesize > MAX_BYTES
-        block = pack(buf)
+        block = pack(buf, level)
         f.write(block)
         @buf = ''
+        @buf_write_pos = tell
       end
+    end
+
+    # @api private
+    def _cur_write_pos
+      @buf_write_pos + buf.bytesize
     end
 
     def write(s)
       if s.bytesize > MAX_BYTES
         write_buf
+        @last_write_pos = _cur_write_pos
         _each_slice(s) do |slice|
           write(slice)
         end
@@ -40,6 +55,7 @@ module Bio::BGZF
         if (s.bytesize + buf.bytesize) > MAX_BYTES
           write_buf
         end
+        @last_write_pos = _cur_write_pos
         buf << s
       end
     end
